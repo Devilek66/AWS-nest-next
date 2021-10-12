a2d39e131b399dd7f0e202e73af294025e9c845f
Aleksander <aleksanderp1993@gmail.com>
2021-10-12 21:17:50 +0200

    wip

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -16,5 +16,8 @@ npm install
 
 #start our node app in the background
 npm run build
-pm2 --name HelloWorld start npm run -- start:prod
+pm2 start npm --name "HelloWorld"
+#pm2 --name HelloWorld start npm -- run start:prod
+#pm2 start npm --name "my-app-name" -- run "start:prod"
+#pm2 start npm --name "app name" -- start
 #node app.js > app.out.log 2> app.err.log < /dev/null & 
