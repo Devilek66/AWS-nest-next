c554c77ef1b04cb8959ce18e054cd0c63213d4a9
Aleksander <aleksanderp1993@gmail.com>
2021-10-12 22:07:49 +0200

    test

--- Backend/nest-test-app/src/app.service.ts
+++ Backend/nest-test-app/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! z pełnym pipelinem baby :D';
+    return 'Hello World! z pełnym pipelinem baby :D itest nr 2';
   }
 }
