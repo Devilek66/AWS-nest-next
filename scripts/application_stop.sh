4f260beaa7f230e8c96e5752a65450c0b36fc489
Aleksander <aleksanderp1993@gmail.com>
2021-10-12 22:03:11 +0200

    xd

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Stopping existing node servers
 # echo "Stopping any existing node servers xd"
-# pm2 stop all
+# pm2 stop all  
 #if pgrep node; then pkill node; fi
 #pkill -x node   
